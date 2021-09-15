e4fb425db8db6da8f04a529665beeeb2be1c3d5c
Anup kumar <anupkumar9344@gmail.com>
2021-09-15 14:29:27 +0530

    Add function convert text

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,10 +7,16 @@ export const TextForm = (props) => {
         setText(newText);
 
     }
+    const handleDownClick =()=>{
+        // console.log("Upercase");
+        let newText = text.toLowerCase();
+        setText(newText);
+
+    }
     const handleonClick =(event)=>{
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here')
+    const [text, setText] = useState('')
     return (
         <div className="mt-5">
             <h1>{props.heading}</h1>
@@ -18,8 +24,17 @@ export const TextForm = (props) => {
 
                 <textarea className="form-control" value={text} onChange={handleonClick} id="myBox" rows="8"></textarea>
             </div>
-            <button onClick={handleUpClick} className="btn btn-info text-white mt-5 d-flex justify-content-center">Convert to Upercase</button>
-
+            <span className="d-flex justify-content-center m-3">
+                <button onClick={handleUpClick} className="btn btn-info text-white m-2 d-flex justify-content-center">Convert to Upercase</button>
+                <button onClick={handleDownClick} className="btn btn-info text-white m-2 d-flex justify-content-center">Convert to Lowercase</button>
+            </span>
+        <div className="container-fluid my-3">
+            <h1 className="mt-5 mb-1">Summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters.</p>
+            <p>{0.008 * text.split(" ").length} Minutes read.</p>
+            <h1 className="mt-5 mb-1">Preview</h1>
+            <p>{text}</p>
+        </div>
         </div>
     )
 }
